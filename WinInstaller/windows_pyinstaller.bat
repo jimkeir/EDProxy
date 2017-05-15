991f2385982243c10a5d6bd2703c6546211060af
jimkeir <jimkeir@yahoo.co.uk>
2017-05-15 15:46:47 +0100

    Add MSI installation for Visual Studio

---
+++ WinInstaller/windows_pyinstaller.bat
@@ -0,0 +1,29 @@
+cd ..
+
+set PATH=%PATH%;C:\Program Files (x86)\Python;C:\Program Files (x86)\Python\Scripts
+set TEMPDIR=%TEMP%\EDProxyBuild
+set OUTPUTEXE=.\Release\EDProxy.exe
+
+echo CD : %CD%
+
+if exist "%TEMPDIR%" (
+	rmdir /Q /S "%TEMPDIR%"
+)
+mkdir "%TEMPDIR%" || goto :error
+
+if exist "%OUTPUTEXE%" (
+	del /Q "%OUTPUTEXE%" || goto :error
+)
+
+pyinstaller --clean --distpath .\Release --workpath "%TEMPDIR%" -y -w --icon=edicon.ico --onefile -n EDProxy edproxy.py || goto :error
+
+REM "C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /i "Comodo" /v /d "EDProxy" /du http://www.lightroom-plugins.com/ /tr http://timestamp.digicert.com .\Release\EDProxy.exe || goto :error
+
+rmdir /Q /S "%TEMPDIR%"
+
+exit /b 0
+goto :EOF
+
+:error
+echo Installation script failed RC %ERRORCODE%
+exit /b %ERRORCODE%
