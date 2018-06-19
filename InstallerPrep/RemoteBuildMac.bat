dc2064a96c0a4dfb4ebac3fab3b5dfb8e97feb63
jimkeir <jimkeir@yahoo.co.uk>
2018-06-19 12:24:31 +0100

    Add remote-build step for creating DMG

---
+++ InstallerPrep/RemoteBuildMac.bat
@@ -0,0 +1,5 @@
+set VERSION=2.4.0
+set SSH_CRED=jimkeir@macmini
+
+"C:\Program Files\Git\usr\bin\ssh.exe" %SSH_CRED% "cd ~/EDProxy && rm -f *.dmg && git pull && PATH=$PATH:~/Library/Python/2.7/bin:/usr/local/bin ./macosx-pyinstall.sh --version %VERSION%"
+"C:\Program Files\Git\usr\bin\scp.exe" -p %SSH_CRED%:~/EDProxy/edproxy-macosx-%VERSION%.dmg ..\Release\
