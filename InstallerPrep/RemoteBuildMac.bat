773a9a0238d1275d5e47654f732d682df78e55ee
jimkeir <jimkeir@yahoo.co.uk>
2018-07-18 15:50:57 +0100

    Version changes only.

--- InstallerPrep/RemoteBuildMac.bat
+++ InstallerPrep/RemoteBuildMac.bat
@@ -1,4 +1,4 @@
-set VERSION=2.4.0
+set VERSION=2.4.1
 set SSH_CRED=jimkeir@macmini
 
 "C:\Program Files\Git\usr\bin\ssh.exe" %SSH_CRED% "cd ~/EDProxy && rm -f *.dmg && git pull && PATH=$PATH:~/Library/Python/2.7/bin:/usr/local/bin ./macosx-pyinstall.sh --version %VERSION%"
